b2afdafba017b761620a5b0f78725fd40e6786a6
SotoiGhost <issoto@microsoft.com>
2018-07-27 17:19:32 -0500

    [Task655407][iOS][LoginKit] Upgraded to support iOS 8 and later

--- Facebook.iOS/LoginKit/source/Facebook.LoginKit/FBSDKLoginKit.linkwith.cs
+++
@@ -1,7 +0,0 @@
-using ObjCRuntime;
-
-[assembly: LinkWith ("FBSDKLoginKit.a",
-		     LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
-		     LinkerFlags = "-ObjC",
-		     SmartLink = true,
-		     ForceLoad = true)]
