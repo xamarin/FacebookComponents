b065083feba8542b17a9fd0b30fcc9c9411c9ee8
Redth <jondick@gmail.com>
2018-09-11 21:47:22 -0400

    Remove obsoleted addition causing dup type
    This should fix #80

--- Facebook.Android/source/facebook-common/additions/additions.cs
+++ Facebook.Android/source/facebook-common/additions/additions.cs
@@ -2,17 +2,6 @@ using Android.Runtime;
 using System;
 using System.Linq;
 
-namespace Xamarin.Facebook
-{
-	public partial class GraphRequestAsyncTask : global::Android.OS.AsyncTask
-	{
-		protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
-		{
-			return new Java.Lang.Object(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "doInBackground", "([Ljava/lang/Object;)Ljava/lang/Object;")), JniHandleOwnership.TransferLocalRef);
-		}
-	}
-}
-
 namespace Xamarin.Facebook.Share.Model
 {
 	public partial class ShareContent
@@ -48,7 +37,6 @@ namespace Xamarin.Facebook.Share.Model
 			}
 		}
 	}
-
 
 	public partial class ShareMedia
 	{
