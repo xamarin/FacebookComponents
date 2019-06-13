fd19627aefbc46a207258b8aefe8f08113cd14b3
SotoiGhost <issoto@microsoft.com>
2019-06-13 17:48:44 -0500

    [iOS][AudienceNetwork] Updated to version 5.3.2

--- Facebook.iOS/samples/FBAudienceNetworkSample/AdPlacementIds.cs
+++ Facebook.iOS/samples/FBAudienceNetworkSample/AdPlacementIds.cs
@@ -4,16 +4,16 @@ using Facebook.AudienceNetwork;
 
 namespace FBAudienceNetworkSample {
 	public static class AdPlacementIds {
-		public const string Banner = "765057006871425_1663886213655162";
-		public const string Interstitial = "765057006871425_1663885900321860";
-		public const string Native = "765057006871425_1663884776988639";
+		public const string Banner = "Banner_Placement_Id";
+		public const string Interstitial = "Interstitial_Placement_Id";
+		public const string Native = "Interstitial_Placement_Id";
 		public const string RewardedVideo = "Rewarded_Video_Placement_Id";
 
 		public static void AddTestDevices ()
 		{
 			AdSettings.LogLevel = AdLogLevel.Log;
 			AdSettings.ClearTestDevices ();
-			AdSettings.AddTestDevice ("e8ce6015a7efafb41de74ab35bc0674c38e58304");
+			AdSettings.AddTestDevice ("Hash_Device");
 		}
 	}
 }
