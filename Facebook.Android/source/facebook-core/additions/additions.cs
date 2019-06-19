6f67f3eeb60e0fd39116caede2189700baa8e27a
SotoiGhost <issoto@microsoft.com>
2019-06-19 10:26:15 -0500

    [Android][core] Updated to version 5.0.3

--- Facebook.Android/source/facebook-core/additions/additions.cs
+++ Facebook.Android/source/facebook-core/additions/additions.cs
@@ -3,12 +3,10 @@ using Android.Runtime;
 
 namespace Xamarin.Facebook
 {
-
-	public partial class GraphRequestAsyncTask
-	{      
-		protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
-        {
-            return new Java.Lang.Object(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "doInBackground", "([Ljava/lang/Object;)Ljava/lang/Object;")), JniHandleOwnership.TransferLocalRef);
-        }
+	public partial class GraphRequestAsyncTask {
+		protected override Java.Lang.Object DoInBackground (params Java.Lang.Object [] @params)
+		{
+			return new Java.Lang.Object (JNIEnv.CallObjectMethod (Handle, JNIEnv.GetMethodID (JNIEnv.GetObjectClass (Handle), "doInBackground", "([Ljava/lang/Object;)Ljava/lang/Object;")), JniHandleOwnership.TransferLocalRef);
+		}
 	}
 }
