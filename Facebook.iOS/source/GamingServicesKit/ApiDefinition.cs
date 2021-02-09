7437279ff41d8a085b6196b738edcaef5b853fde
thisisthekap <c.kapplmueller@me.com>
2021-02-09 13:26:10 +0100

    removed FBSDK prefix from gaming service component names

--- Facebook.iOS/source/GamingServicesKit/ApiDefinition.cs
+++ Facebook.iOS/source/GamingServicesKit/ApiDefinition.cs
@@ -6,60 +6,60 @@ using UIKit;
 namespace Facebook.GamingServiceKit
 {
     // typedef void (^FBSDKGamingServiceCompletionHandler)(BOOL, NSError * _Nullable);
-    delegate void FBSDKGamingServiceCompletionHandler(bool arg0, [NullAllowed] NSError arg1);
+    delegate void GamingServiceCompletionHandler(bool arg0, [NullAllowed] NSError arg1);
 
     // typedef void (^FBSDKGamingServiceResultCompletionHandler)(BOOL, NSString * _Nullable, NSError * _Nullable);
-    delegate void FBSDKGamingServiceResultCompletionHandler(bool arg0, [NullAllowed] string arg1, [NullAllowed] NSError arg2);
+    delegate void GamingServiceResultCompletionHandler(bool arg0, [NullAllowed] string arg1, [NullAllowed] NSError arg2);
 
     // typedef void (^FBSDKGamingServiceProgressHandler)(int64_t, int64_t, int64_t);
-    delegate void FBSDKGamingServiceProgressHandler(long arg0, long arg1, long arg2);
+    delegate void GamingServiceProgressHandler(long arg0, long arg1, long arg2);
 
     // @interface FBSDKFriendFinderDialog : NSObject
-    [BaseType(typeof(NSObject))]
+    [BaseType(typeof(NSObject), Name = "FBSDKFriendFinderDialog")]
     [DisableDefaultCtor]
-    interface FBSDKFriendFinderDialog
+    interface FriendFinderDialog
     {
         // +(void)launchFriendFinderDialogWithCompletionHandler:(FBSDKGamingServiceCompletionHandler _Nonnull)completionHandler;
         [Static]
         [Export("launchFriendFinderDialogWithCompletionHandler:")]
-        void LaunchFriendFinderDialogWithCompletionHandler(FBSDKGamingServiceCompletionHandler completionHandler);
+        void LaunchFriendFinderDialogWithCompletionHandler(GamingServiceCompletionHandler completionHandler);
     }
 
     // @interface FBSDKGamingGroupIntegration : NSObject
-    [BaseType(typeof(NSObject))]
-    interface FBSDKGamingGroupIntegration
+    [BaseType(typeof(NSObject), Name = "FBSDKGamingGroupIntegration")]
+    interface GamingGroupIntegration
     {
         // +(void)openGroupPageWithCompletionHandler:(FBSDKGamingServiceCompletionHandler _Nonnull)completionHandler;
         [Static]
         [Export("openGroupPageWithCompletionHandler:")]
-        void OpenGroupPageWithCompletionHandler(FBSDKGamingServiceCompletionHandler completionHandler);
+        void OpenGroupPageWithCompletionHandler(GamingServiceCompletionHandler completionHandler);
     }
 
     // @interface FBSDKGamingImageUploader : NSObject
-    [BaseType(typeof(NSObject))]
+    [BaseType(typeof(NSObject), Name = "FBSDKGamingImageUploader")]
     [DisableDefaultCtor]
-    interface FBSDKGamingImageUploader
+    interface GamingImageUploader
     {
         // +(void)uploadImageWithConfiguration:(FBSDKGamingImageUploaderConfiguration * _Nonnull)configuration andCompletionHandler:(FBSDKGamingServiceCompletionHandler _Nonnull)completionHandler __attribute__((deprecated("Please use uploadImageWithConfiguration:andResultCompletionHandler: instead")));
         [Static]
         [Export("uploadImageWithConfiguration:andCompletionHandler:")]
-        void UploadImageWithConfiguration(FBSDKGamingImageUploaderConfiguration configuration, FBSDKGamingServiceCompletionHandler completionHandler);
+        void UploadImageWithConfiguration(GamingImageUploaderConfiguration configuration, GamingServiceCompletionHandler completionHandler);
 
         // +(void)uploadImageWithConfiguration:(FBSDKGamingImageUploaderConfiguration * _Nonnull)configuration andResultCompletionHandler:(FBSDKGamingServiceResultCompletionHandler _Nonnull)completionHandler;
         [Static]
         [Export("uploadImageWithConfiguration:andResultCompletionHandler:")]
-        void UploadImageWithConfiguration(FBSDKGamingImageUploaderConfiguration configuration, FBSDKGamingServiceResultCompletionHandler completionHandler);
+        void UploadImageWithConfiguration(GamingImageUploaderConfiguration configuration, GamingServiceResultCompletionHandler completionHandler);
 
         // +(void)uploadImageWithConfiguration:(FBSDKGamingImageUploaderConfiguration * _Nonnull)configuration completionHandler:(FBSDKGamingServiceResultCompletionHandler _Nonnull)completionHandler andProgressHandler:(FBSDKGamingServiceProgressHandler _Nullable)progressHandler;
         [Static]
         [Export("uploadImageWithConfiguration:completionHandler:andProgressHandler:")]
-        void UploadImageWithConfiguration(FBSDKGamingImageUploaderConfiguration configuration, FBSDKGamingServiceResultCompletionHandler completionHandler, [NullAllowed] FBSDKGamingServiceProgressHandler progressHandler);
+        void UploadImageWithConfiguration(GamingImageUploaderConfiguration configuration, GamingServiceResultCompletionHandler completionHandler, [NullAllowed] GamingServiceProgressHandler progressHandler);
     }
 
     // @interface FBSDKGamingImageUploaderConfiguration : NSObject
-    [BaseType(typeof(NSObject))]
+    [BaseType(typeof(NSObject), Name = "FBSDKGamingImageUploaderConfiguration")]
     [DisableDefaultCtor]
-    interface FBSDKGamingImageUploaderConfiguration
+    interface GamingImageUploaderConfiguration
     {
         // @property (readonly, nonatomic, strong) UIImage * _Nonnull image;
         [Export("image", ArgumentSemantic.Strong)]
@@ -79,30 +79,30 @@ namespace Facebook.GamingServiceKit
     }
 
     // @interface FBSDKGamingVideoUploader : NSObject
-    [BaseType(typeof(NSObject))]
+    [BaseType(typeof(NSObject), Name = "FBSDKGamingVideoUploader")]
     [DisableDefaultCtor]
-    interface FBSDKGamingVideoUploader
+    interface GamingVideoUploader
     {
         // +(void)uploadVideoWithConfiguration:(FBSDKGamingVideoUploaderConfiguration * _Nonnull)configuration andCompletionHandler:(FBSDKGamingServiceCompletionHandler _Nonnull)completionHandler __attribute__((deprecated("Please use uploadVideoWithConfiguration:andResultCompletionHandler: instead")));
         [Static]
         [Export("uploadVideoWithConfiguration:andCompletionHandler:")]
-        void UploadVideoWithConfiguration(FBSDKGamingVideoUploaderConfiguration configuration, FBSDKGamingServiceCompletionHandler completionHandler);
+        void UploadVideoWithConfiguration(GamingVideoUploaderConfiguration configuration, GamingServiceCompletionHandler completionHandler);
 
         // +(void)uploadVideoWithConfiguration:(FBSDKGamingVideoUploaderConfiguration * _Nonnull)configuration andResultCompletionHandler:(FBSDKGamingServiceResultCompletionHandler _Nonnull)completionHandler;
         [Static]
         [Export("uploadVideoWithConfiguration:andResultCompletionHandler:")]
-        void UploadVideoWithConfiguration(FBSDKGamingVideoUploaderConfiguration configuration, FBSDKGamingServiceResultCompletionHandler completionHandler);
+        void UploadVideoWithConfiguration(GamingVideoUploaderConfiguration configuration, GamingServiceResultCompletionHandler completionHandler);
 
         // +(void)uploadVideoWithConfiguration:(FBSDKGamingVideoUploaderConfiguration * _Nonnull)configuration completionHandler:(FBSDKGamingServiceResultCompletionHandler _Nonnull)completionHandler andProgressHandler:(FBSDKGamingServiceProgressHandler _Nullable)progressHandler;
         [Static]
         [Export("uploadVideoWithConfiguration:completionHandler:andProgressHandler:")]
-        void UploadVideoWithConfiguration(FBSDKGamingVideoUploaderConfiguration configuration, FBSDKGamingServiceResultCompletionHandler completionHandler, [NullAllowed] FBSDKGamingServiceProgressHandler progressHandler);
+        void UploadVideoWithConfiguration(GamingVideoUploaderConfiguration configuration, GamingServiceResultCompletionHandler completionHandler, [NullAllowed] GamingServiceProgressHandler progressHandler);
     }
 
     // @interface FBSDKGamingVideoUploaderConfiguration : NSObject
-    [BaseType(typeof(NSObject))]
+    [BaseType(typeof(NSObject), Name = "FBSDKGamingVideoUploaderConfiguration")]
     [DisableDefaultCtor]
-    interface FBSDKGamingVideoUploaderConfiguration
+    interface GamingVideoUploaderConfiguration
     {
         // @property (readonly, nonatomic, strong) NSURL * _Nonnull videoURL;
         [Export("videoURL", ArgumentSemantic.Strong)]
@@ -118,14 +118,14 @@ namespace Facebook.GamingServiceKit
     }
 
     [Static]
-    partial interface Constants
+    partial interface GamingServicesConstants
     {
         // extern double FBSDKGamingServicesKitVersionNumber;
         [Field("FBSDKGamingServicesKitVersionNumber", "__Internal")]
-        double FBSDKGamingServicesKitVersionNumber { get; }
+        double GamingServicesKitVersionNumber { get; }
 
         // extern const unsigned char [] FBSDKGamingServicesKitVersionString;
         [Field("FBSDKGamingServicesKitVersionString", "__Internal")]
-        NSString FBSDKGamingServicesKitVersionString { get; }
+        NSString GamingServicesKitVersionString { get; }
     }
 }
